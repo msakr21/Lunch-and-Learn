1ca144fb7dcf7ffece027bb2c67f19fa260e2876
Mostafa Sakr <msakr3@gmail.com>
2023-01-16 21:56:57 -0700

    Add random country generation in controller.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RecipesController < ApplicationController 
   def index
     country = params[:country]
-    country = "api_call" unless country
+    country = CountriesService.get_list.sample[:name] if country == ""
     recipe = RecipeFacade.recipe_info(country)
     render json: RecipeSerializer.new(recipe)
   end

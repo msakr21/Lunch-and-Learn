62ecfe33b40fff53d95524f770254f057b3eceae
Mostafa Sakr <msakr3@gmail.com>
2023-01-16 21:48:57 -0700

    Add test and implementation code for countries service.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecipesController < ApplicationController 
   def index
     country = params[:country]
+    country = "api_call" unless country
     recipe = RecipeFacade.recipe_info(country)
     render json: RecipeSerializer.new(recipe)
   end
---
+++ spec/services/countries_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe CountriesService do 
+    it '.get_recipe' do 
+        response = CountriesService.get_list
+
+        expect(response).to be_a Array
+        expect(response.first.keys).to eq([:name, :independent])
+        expect(response.first[:name]).to eq("Afghanistan")
+    end 
+end 

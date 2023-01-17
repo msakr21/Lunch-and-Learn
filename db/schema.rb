8c17084f448ceea33a8c07b78407c6d149bf4a9d
Mostafa Sakr <msakr3@gmail.com>
2023-01-17 15:10:40 -0700

    Add user_id foreign key to favorites table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_211242) do
+ActiveRecord::Schema.define(version: 2023_01_17_220906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2023_01_17_211242) do
     t.string "recipe_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2023_01_17_211242) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "users"
 end

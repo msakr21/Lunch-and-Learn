1efaa4595baefce6920881ef01b8dc3a37aeee25
Mostafa Sakr <msakr3@gmail.com>
2023-01-17 03:52:00 -0700

    Add test and implementation code for request.

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,4 +1,7 @@
-class Api::V1::RecipesController < ApplicationController 
+class Api::V1::LearningResourcesController < ApplicationController 
   def show
+    country = params[:country]
+    learning_resource = LearningResourceFacade.resource_list(country)
+    render json: LearningResourceSerializer.new(learning_resource)
   end
 end

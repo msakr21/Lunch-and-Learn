7a0c7d264033222febfd4736f9b08b69ad7706c6
Mostafa Sakr <msakr3@gmail.com>
2023-01-18 01:34:17 -0700

    Update git ignore and some other minor fixes.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::RecipesController < ApplicationController
     country = params[:country]
     country = CountriesService.get_list.sample[:name] unless country 
     recipe = RecipeFacade.recipe_info(country)
+    # binding.pry
     if recipe.nil? 
       render json: {"data": []}
     else
